5447b6ccd80ea199f2b611e8e4915ea26b84acf0
tomaslingotti <tomaslingotti@gmail.com>
2020-04-06 15:51:37 -0300

    add port env variable

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/tomiok/minesweeper-API/api"
 	"github.com/tomiok/minesweeper-API/internal/logs"
+	"os"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	serverPort := os.Getenv("PORT")
 	logs.InitDefault("dev") //hardcoded at dev environment for the PoC API
-	api.Start(defaultPort)
+	if serverPort == "" {
+		serverPort = defaultPort
+	}
+	api.Start(serverPort)
 }
