2aba7498b4aa68b94b3af995f50220d03c77f50e
tomaslingotti <tomaslingotti@gmail.com>
2020-04-26 18:57:39 -0300

    add env variable

--- cmd/main.go
+++ cmd/main.go
@@ -10,9 +10,11 @@ const defaultPort = "8080"
 
 func main() {
 	serverPort := os.Getenv("PORT")
+	testEnvVariable := os.Getenv("TEST")
 	logs.InitDefault("dev") //hardcoded at dev environment for the PoC API
 	if serverPort == "" {
 		serverPort = defaultPort
 	}
+	logs.Log().Info(testEnvVariable)
 	api.Start(serverPort)
 }
